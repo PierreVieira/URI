daec27093342c68110dc12326b3a124574d37aaa
PierreVieira <pierrevieiraggg@gmail.com>
2019-05-29 23:04:20 -0300

    Sem solução por enquanto

--- 2448 - Carteiro.py
+++ 2448 - Carteiro.py
@@ -1,4 +1,4 @@
-#Pierre Vieira 29/05/2019
+#Pierre Vieira 29/05/2019 - Ainda temos que resolver
 primeira_linha = input().split()
 segunda_linha = input().split()
 terceira_linha = input().split()
@@ -6,8 +6,7 @@ casas = list(map(int, segunda_linha))
 entregas = list(map(int, terceira_linha))
 minutos = 0
 for c in entregas:
-    for d in range(len(casas)):
-        if(c == casas[d]):
-            break
-        minutos += 1
-print(minutos)
+    for d in casas:
+        if c != d:
+            minutos += 1
+print(int(minutos/2))
