40f8a85771319c8663a114f7f5eeb6778642157f
Pierre Vieira <49538805+PierreVieira@users.noreply.github.com>
2019-10-27 10:17:37 -0300

    Create 1008 - Salário.py
    https://www.urionlinejudge.com.br/judge/pt/problems/view/1008

---
+++ Iniciante/1008 - Salário.py
@@ -0,0 +1,8 @@
+"""
+Autor: Pierre Vieira
+Data da submissão: 16/01/2019 23:47:36
+"""
+numero_funcionario = int(input())
+qtde_horas = int(input())
+valor_trabalho = float(input())
+print("NUMBER = {}\nSALARY = U$ {:.2f}".format(numero_funcionario, qtde_horas*valor_trabalho))
