e302856193b277f806e7d5ef6dacbd00423556ee
Pierre Vieira <49538805+PierreVieira@users.noreply.github.com>
2019-12-17 13:22:26 -0300

    Create  2164 - Fibonacci Rápido.py

---
+++ Iniciante/2164 - Fibonacci Rápido.py
@@ -0,0 +1,7 @@
+"""
+Autor: Pierre Vieira
+Data da submissão: 24/02/2019 23:14:43
+"""
+n = int(input())
+resultado = (((((1+(5**0.5))/2)**n)-(((1-(5**0.5))/2)**n))/5**0.5)
+print('{:.1f}'.format(resultado))
