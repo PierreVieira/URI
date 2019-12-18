c4b36b9fdfead0eaeb87ac07ea6134e22a7e0db4
Pierre Vieira <49538805+PierreVieira@users.noreply.github.com>
2019-12-18 15:21:37 -0300

    Create 1050 - DDD.py

---
+++ Iniciante/1050 - DDD.py
@@ -0,0 +1,23 @@
+"""
+Autor: Pierre Vieira
+Data da submissão: 24/01/2019 22:03:34
+"""
+n = int(input())
+if n == 61:
+    print('Brasilia')
+elif n == 71:
+    print('Salvador')
+elif n == 11:
+    print("Sao Paulo")
+elif n == 21:
+    print("Rio de Janeiro")
+elif n == 32:
+    print("Juiz de Fora")
+elif n == 19:
+    print("Campinas")
+elif n == 27:
+    print("Vitoria")
+elif n == 31:
+    print("Belo Horizonte")
+else:
+    print("DDD nao cadastrado")
